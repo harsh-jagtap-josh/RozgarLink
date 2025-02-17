f0ba366282f1e71e4a025280993417abc55d3492
harsh-jagtap-josh <harsh.jagtap@joshsoftware.com>
2025-02-17 23:31:11 +0530

    add const group for employer db queries

--- internal/repo/employer.go
+++ internal/repo/employer.go
@@ -18,6 +18,16 @@ type EmployerStorer interface {
 	FindEmployerById(ctx context.Context, employerId int) bool
 }
 
+// PostgreSQL Queries
+const (
+	registerWorkerQuery      = `INSERT INTO employers (name, contact_number, email, type, password, sectors, location, is_verified, rating, workers_hired, created_at, updated_at, language) VALUES (:name, :contact_number, :email, :type, :password, :sectors, :location, :is_verified, :rating, :workers_hired, NOW(), NOW(), :language) RETURNING *;`
+	fetchEmployerByIDQuery   = `SELECT employers.*, address.details, address.street, address.city, address.state, address.pincode from employers inner join address on employers.location = address.id where employers.id = $1;`
+	updateEmployerByIdQuery  = `UPDATE employers SET name=:name, contact_number=:contact_number, email=:email, type=:type, sectors=:sectors, rating=:rating, workers_hired=:workers_hired, is_verified=:is_verified, updated_at=NOW(), language=:language WHERE id=:id RETURNING *;`
+	deleteEmployerByIdQuery  = `DELETE from employers where id=$1 RETURNING location;`
+	findEmployerByEmailQuery = `SELECT id from employers where email=$1;`
+	findEmployerByIDQuery    = `SELECT id from employers where id=$1;`
+)
+
 type employerStore struct {
 	BaseRepository
 }
@@ -47,9 +57,7 @@ func (es *employerStore) RegisterEmployer(ctx context.Context, employerData Empl
 
 	employerData.Location = address.ID
 
-	query := `INSERT INTO employers (name, contact_number, email, type, password, sectors, location, is_verified, rating, workers_hired, created_at, updated_at, language) VALUES (:name, :contact_number, :email, :type, :password, :sectors, :location, :is_verified, :rating, :workers_hired, NOW(), NOW(), :language) RETURNING *;`
-
-	rows, err := db.NamedQuery(query, employerData)
+	rows, err := db.NamedQuery(registerWorkerQuery, employerData)
 	if err != nil {
 		return EmployerResponse{}, err
 	}
@@ -70,9 +78,8 @@ func (es *employerStore) FetchEmployerByID(ctx context.Context, employerId int) 
 	db := sqlx.NewDb(es.DB, "postgres")
 
 	var employer EmployerResponse
-	query := `SELECT employers.*, address.details, address.street, address.city, address.state, address.pincode from employers inner join address on employers.location = address.id where employers.id = $1;`
 
-	err := db.Get(&employer, query, employerId)
+	err := db.Get(&employer, fetchEmployerByIDQuery, employerId)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return EmployerResponse{}, apperrors.ErrNoEmployerExists
@@ -108,8 +115,7 @@ func (es *employerStore) UpdateEmployerById(ctx context.Context, employerData Em
 		}
 	}
 
-	query := `UPDATE employers SET name=:name, contact_number=:contact_number, email=:email, type=:type, sectors=:sectors, rating=:rating, workers_hired=:workers_hired, is_verified=:is_verified, updated_at=NOW(), language=:language WHERE id=:id RETURNING *;`
-	rows, err := db.NamedQuery(query, employerData)
+	rows, err := db.NamedQuery(updateEmployerByIdQuery, employerData)
 	if err != nil {
 		return EmployerResponse{}, err
 	}
@@ -131,12 +137,11 @@ func (es *employerStore) UpdateEmployerById(ctx context.Context, employerData Em
 }
 
 func (es *employerStore) DeleteEmployerByID(ctx context.Context, employerId int) (int, error) {
-	query := `DELETE from employers where id=$1 RETURNING location;`
 
 	db := sqlx.NewDb(es.DB, "postgres")
 	var addressId int
 
-	err := db.Get(&addressId, query, employerId)
+	err := db.Get(&addressId, deleteEmployerByIdQuery, employerId)
 
 	if err != nil {
 		return -1, err
@@ -151,14 +156,13 @@ func (es *employerStore) DeleteEmployerByID(ctx context.Context, employerId int)
 
 func (es *employerStore) FindEmployerByEmail(ctx context.Context, employerEmail string) bool {
 	var ID int
-	query := `SELECT id from employers where email=$1;`
-	err := es.BaseRepository.DB.QueryRow(query, employerEmail).Scan(&ID)
+
+	err := es.BaseRepository.DB.QueryRow(findEmployerByEmailQuery, employerEmail).Scan(&ID)
 	return err == nil
 }
 
 func (es *employerStore) FindEmployerById(ctx context.Context, employerId int) bool {
 	var ID int
-	query := `SELECT id from employers where id=$1;`
-	err := es.BaseRepository.DB.QueryRow(query, employerId).Scan(&ID)
+	err := es.BaseRepository.DB.QueryRow(findEmployerByIDQuery, employerId).Scan(&ID)
 	return err == nil
 }
